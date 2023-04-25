8f806bc8b08c327efb796339871063e9e4ca5b03
Arien Shibani <aaari94@gmail.com>
2023-04-25 17:10:41 +0200

    feat(App.js): :sparkles:

--- src/App.js
+++ src/App.js
@@ -4,19 +4,73 @@ import { useEffect, useState } from "react"
 import { championMapping } from './util/championMasteryMapper';
 
 
-function renderMasteryObject(masteryObject) {
+function renderMasteryObject(el) {
   return (
-    <div style={{backgroundColor: "pink", fontFamily: "Comic Sans MS"}} key={masteryObject.championId + masteryObject.championPoints + masteryObject.lastPlayTime}>
-           <p>Champion ID: {championMapping[masteryObject.championId]}</p>
-           <p>Points: {masteryObject.championPoints}</p>
-         </div>
-    )
+    <div class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 m-4 ">
+      <div class="flex items-center justify-between mb-4">
+        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">{championMapping[el.championId]}</h5>
+      </div>
+
+      <div class="flex flex-col items-center">
+        <ul class="divide-y divide-gray-200 dark:divide-gray-700">
+          <li class="py-3 sm:py-4">
+            <div class="flex items-center space-x-4">
+              <div class="flex-shrink-0">
+
+              </div>
+              <div class="flex-1 min-w-0">
+                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                  Mastery
+                </p>
+              </div>
+              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                {el.championPoints}
+              </div>
+            </div>
+          </li>
+
+          <li class="py-3 sm:py-4">
+            <div class="flex items-center space-x-4">
+              <div class="flex-shrink-0">
+
+              </div>
+              <div class="flex-1 min-w-0">
+                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                  Champion Level
+                </p>
+              </div>
+              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                {el.championLevel}
+              </div>
+            </div>
+          </li>
+
+          <li class="py-3 sm:py-4">
+            <div class="flex items-center space-x-4">
+              <div class="flex-shrink-0">
+
+              </div>
+              <div class="flex-1 min-w-0">
+                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                  Last Played
+                </p>
+              </div>
+              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                {new Date(el.lastPlayTime).toLocaleString()}
+              </div>
+            </div>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 
 function App() {
   const [data, setData] = useState(null);
-  const API_KEY = "RGAPI-9bf19606-62c6-46e1-9ea0-caed51d16de4"
 
   const getSummonerMasteries = (accountId) => {
     let response;
@@ -33,20 +87,24 @@ function App() {
     getSummonerMasteries("5YF-SXzQ8h3AlfDOmQ_uxXp7QECXL70xJKeQjuGd0PtNoXU")
   }, [])
 
-  console.log(data)
+
 
   return (
     <>
       <div className="App">
-        <h1>Kespestatus?</h1>
-        <p>Once a briliant child in his domain, everything changed after the military. The kassernå was the place where he got introduced to 'blodbuging med forhuden'.
+      <img src="https://akamai.vgc.no/v2/images/2911467c-2e4a-42a1-a33b-53a8a96aeb08?fit=crop&format=auto&h=1267&w=1900&s=b7eb326cd7846d781e19cff37bfc4e7a00e1b2a1"></img>
+
+        <h1 class="text-3xl">Kespestatus?</h1>
+        <p class="text-base">Once a briliant child in his domain, everything changed after the military. The kassernå was the place where he got introduced to 'blodbuging med forhuden'.
           A traditional term in the military, where every boy gathers in the kassernå and puts a 'kespe' in their 'forhud'.
           Once the tradition was learned, the child wandered through 'helvetesukå' and dimmmed in the mountain of blodbug.
           According to the legend, this is where the infant was forged to become Kespejones.
           Gutta glemme aldri dimmeturen til svalbard. </p>
       </div>
-      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
 
+      <br></br>
+      <h1 class="text-3xl">BPK DOGCAT</h1>
+      
       <div>
         {data && data.map((masteryObject) => renderMasteryObject(masteryObject))}
       </div>
