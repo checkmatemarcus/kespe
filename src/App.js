d688b739d069b6b3cf7d2992a7e9a75035543850
Markus <checkmatemarcus@gmail.com>
2023-04-25 13:38:54 +0200

    feat: :bug: init repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kespestatus?</h1>
+      <p>Once a briliant child in his domain, everything changed after the military. The kassernå was the place where he got introduced to 'blodbuging med forhuden'. 
+        A traditional term in the military, where every boy gathers in the kassernå and puts a 'kespe' in their 'forhud'. 
+        Once the tradition was learned, the child wandered through 'helvetesukå' and dimmmed in the mountain of blodbug. 
+        According to the legend, this is where the infant was forged to become Kespejones. 
+        Gutta glemme aldri dimmeturen til svalgberg. </p>
+    </div>
+  );
+}
+
+export default App;
