6f5492f326b137b3808aaa98156a3824fc39ca7b
Markus <checkmatemarcus@gmail.com>
2023-04-25 16:41:34 +0200

    fix: :bug: added tailwind

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
           According to the legend, this is where the infant was forged to become Kespejones.
           Gutta glemme aldri dimmeturen til svalbard. </p>
       </div>
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
 
       <div>
         {data && data.map((masteryObject) => {
@@ -45,6 +46,7 @@ function App() {
           )
         })}
       </div>
+      
 
 
     </>
