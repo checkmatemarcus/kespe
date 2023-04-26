18ba5f4ca647b1456079888406731acc6a8d08b9
Arien Shibani <aaari94@gmail.com>
2023-04-26 14:45:21 +0200

    beans on toast

--- src/util/riotAPI.js
+++ src/util/riotAPI.js
@@ -1,27 +1,12 @@
-import {Kayn, REGIONS} from "kayn"
+import axios from 'axios';
 
-// Configure settings for Riot API calls.
-export default Kayn(process.env.RIOT_LOL_API_KEY)({
-    region: REGIONS.EUROPE_WEST,
-    apiURLPrefix: 'https://euw1.api.riotgames.com',
-    locale: 'en_US',
-    debugOptions: {
-        isEnabled: true,
-        showKey: false,
-    },
-    requestOptions: {
-        shouldRetry: true,
-        numberOfRetriesBeforeAbort: 2,
-        delayBeforeRetry: 200,
-        burst: false,
-        shouldExitOn403: false,
-    },
-    cacheOptions: {
-        cache: null,
-        timeToLives: {
-            useDefault: false,
-            byGroup: {},
-            byMethod: {},
-        },
-    },
-})
+const getSummonerMasteries = (accountId) => {
+    let response;
+
+    axios.get(`https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${accountId}?api_key=${process.env.API_KEY}`, {
+    }).then((res) => {
+      response = res.data;
+    })
+
+    return response;
+  }
