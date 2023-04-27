772dbe988acec32b3618a083597d481bfeeb1240
Arien Shibani <aaari94@gmail.com>
2023-04-27 16:10:05 +0200

    fixed backend and front end communication.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-import fetch from "node-fetch";
 import express from 'express';
 import cors from 'cors';
+import {getSummonerMasteryByName} from './riotAPI.js';
 
 const PORT = 5000;
 const app = express();
@@ -10,22 +10,17 @@ const corsOptions = {
     origin: "http://localhost:3000"
 };
 
-const riotAPI = "https://euw1.api.riotgames.com/lol"
-
-
-// This function runs if the http://localhost:5000/getData endpoint
-// is requested with a GET request
-app.get('/getData', cors(corsOptions), async (req, res) => {
-    const fetchOptions = {
-        method: 'GET',
-        headers:{
-            "X-Riot-Token": "RGAPI-a6e7e611-b5ed-41b4-84d2-18029e28e0d0"
-        }
-        
+app.get("/getMasteryByName/:name", cors(corsOptions), async (req, res) => {
+    // Querry parameter is the summoner name, retreived from the URL path.
+    const summonerName = req.params.name;
+    let data;
+    
+    try {
+        data = await getSummonerMasteryByName(summonerName);
+    } catch (error) {
+        data = {status: error.response.status, message: error.response.statusText}
     }
-    const response = await fetch(riotAPI, fetchOptions);
-    const jsonResponse = await response.json();
-    res.json(jsonResponse);
+    res.json({data});
 });
 
 app.listen(PORT, () => {
--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { getSummonerMasteryByName } from './util/riotAPI';
 import { useEffect, useState } from "react"
 import { renderMasteryObject } from './components/ChampionMasteryComponent';
-
 import Tabs from './Tabs';
 
 function App() {
@@ -14,73 +13,101 @@ function App() {
   const [balagurbizMastery, setBalagurbizMastery] = useState(null);
   const [modaMastery, setModaMastery] = useState(null);
 
+  // Loading state:
+  const [loading, setLoading] = useState(true);
 
-  const fetchDataAndSetToState = async () => {
-    setKespeJonesMastery(await getSummonerMasteryByName("BPK boobdude"))
-    setDogcatMastery(await getSummonerMasteryByName("BPK DOGCAT"))
-    setSindreMastery(await getSummonerMasteryByName("BPK HAHAHHAHAHAH"))
-    setModaMastery(await getSummonerMasteryByName("BPK Modalitsu"))
-    setBalagurbizMastery(await getSummonerMasteryByName("BPK Balagurbiz"))
-  }
+
 
   useEffect(() => {
-    fetchDataAndSetToState()
+    const fetchDataAndSetToState = async () => {
+
+      const result = (
+        await Promise.all([
+          getSummonerMasteryByName("BPK boobdude"),
+          getSummonerMasteryByName("BPK DOGCAT"),
+          getSummonerMasteryByName("BPK HAHAHHAHAHAH"),
+          getSummonerMasteryByName("BPK Modalitsu"),
+          getSummonerMasteryByName("BPK Balagurbiz")
+        ])
+      )
+
+ 
+
+      setKespeJonesMastery(result[0].data);
+      setDogcatMastery(result[1].data);
+      setSindreMastery(result[2].data);
+      setModaMastery(result[3].data);
+      setBalagurbizMastery(result[4].data);
+
+      setLoading(false);
+
+      console.log(balagurbizMastery)
+    }
+
+    fetchDataAndSetToState();
+
+
   }, [])
 
 
-  return (
-    <>
-      <Tabs>
-        {/* LOTHE */}
-        <div label="Home">
-          <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>Kespe.no is currently undergoing a potential merger with alkulator.no.
-            As a result, the management is allocating most of its resources to mitigate potential confusion between the two parties.
-            New features will be added in the near future. Please bear with us as we work through any bugs that may arise. Stay sturdy with the kesper</p>
-        </div>
+  if (!loading) {
 
-        {/* KESPE JONES */}
-        <div label="Kespejones">
-          <h1 class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>Kespestatus?</h1>
-          <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>Once a briliant child in his domain, everything changed after the military. The kassernå was the place where he got introduced to 'blodbuging med forhuden'.
-            A traditional term in the military, where every boy gathers in the kassernå and puts a 'kespe' in their 'forhud'.
-            Once the tradition was learned, the child wandered through 'helvetesukå' and dimmmed in the mountain of blodbug.
-            According to the legend, this is where the infant was forged to become Kespejones.
-            Gutta glemme aldri dimmeturen til svalbard.
-          </p>
-          {kespejonesMastery && kespejonesMastery.map((masteryObject) => renderMasteryObject(masteryObject))}
-        </div>
-
-        {/* DOGCAT */}
-        <div label="Dogcat">
-          <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>ja, kanskje du skulle ha tatt ein bug i før du ganka top lane?</p>
-          {dogcatMastery && dogcatMastery.map((masteryObject) => renderMasteryObject(masteryObject))}
-        </div>
-
-        {/* BALAGARBAGE */}
-        <div label="Balagorbies">
-          <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>"faen, de banna Kaisa"</p>
-          <div style={{maxHeight: "200px"}}>
-          {balagurbizMastery && balagurbizMastery.map((masteryObject) => renderMasteryObject(masteryObject))}
+    return (
+      <>
+        <Tabs>
+          {/* LOTHE */}
+          <div label="Home">
+            <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>Kespe.no is currently undergoing a potential merger with alkulator.no.
+              As a result, the management is allocating most of its resources to mitigate potential confusion between the two parties.
+              New features will be added in the near future. Please bear with us as we work through any bugs that may arise. Stay sturdy with the kesper</p>
           </div>
-        </div>
+
+          {/* KESPE JONES */}
+          <div label="Kespejones">
+            <h1 class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>Kespestatus?</h1>
+            <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>Once a briliant child in his domain, everything changed after the military. The kassernå was the place where he got introduced to 'blodbuging med forhuden'.
+              A traditional term in the military, where every boy gathers in the kassernå and puts a 'kespe' in their 'forhud'.
+              Once the tradition was learned, the child wandered through 'helvetesukå' and dimmmed in the mountain of blodbug.
+              According to the legend, this is where the infant was forged to become Kespejones.
+              Gutta glemme aldri dimmeturen til svalbard.
+            </p>
+            {kespejonesMastery && kespejonesMastery.data.map((masteryObject) => renderMasteryObject(masteryObject))}
+          </div>
+
+          {/* DOGCAT */}
+          <div label="Dogcat">
+            <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>ja, kanskje du skulle ha tatt ein bug i før du ganka top lane?</p>
+            {dogcatMastery && dogcatMastery.data.map((masteryObject) => renderMasteryObject(masteryObject))}
+          </div>
+
+          {/* BALAGARBAGE */}
+          <div label="Balagorbies">
+            <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>"faen, de banna Kaisa"</p>
+            <div style={{ maxHeight: "200px" }}>
+              {balagurbizMastery && balagurbizMastery.data.map((masteryObject) => renderMasteryObject(masteryObject))}
+            </div>
+          </div>
 
 
-        {/* Modamoda */}
-        <div label="Modamoda">
-          <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>Best support EU</p>
-          {modaMastery && modaMastery.map((masteryObject) => renderMasteryObject(masteryObject))}
-        </div>
+          {/* Modamoda */}
+          <div label="Modamoda">
+            <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>Best support EU</p>
+            {modaMastery && modaMastery.data.map((masteryObject) => renderMasteryObject(masteryObject))}
+          </div>
 
-        {/* Sindre */}
-        <div label="HAHAHAHAH">
-          <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>"har me jungler egentlig?"</p>
-          {sindreMastery && sindreMastery.map((masteryObject) => renderMasteryObject(masteryObject))}
-        </div>
+          {/* Sindre */}
+          <div label="HAHAHAHAH">
+            <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>"har me jungler egentlig?"</p>
+            {sindreMastery && sindreMastery.data.map((masteryObject) => renderMasteryObject(masteryObject))}
+          </div>
 
-      </Tabs>
+        </Tabs>
 
-    </>
-  );
+      </>
+    );
+  }
+
+
 }
 
 export default App;
