3069a4f79a51b682b8de40e52bca8835c530e135
Markus <checkmatemarcus@gmail.com>
2023-05-11 06:02:00 +0200

    style: anti huge

--- src/pages/BPK%20DOGCAT.js
+++ src/pages/BPK%20DOGCAT.js
@@ -8,6 +8,34 @@ export default function boobdude() {
     return (
         <>
             <p class="text-center max-w-xs flex justify-center" style={{ margin: "auto" }}>"ja, kanskje du skulle tatt ein bug i før du ganka top?"</p>
+            <h1>We all have that friend who fucks up the game for everyone else.</h1>
+
+            <p> You've arrived at the right place to get some tips on how you can motivate your friend.
+
+                Inting (intentional feeding)
+                in League of Legends is a serious problem that can negatively impact the gameplay experience for both the player inting and their teammates.
+                Here are some tips to help your friend calm down and perform better in the top lane:
+
+                Encourage him to take a break: If your friend is consistently inting, it's possible that he's just having a bad day, drunk or feeling burnt out from playing too much.
+                Suggest that he take a break from the game for a few hours or even a day or two to refresh his mind and come back with a clearer head.
+
+                he might have enjoyed too many beers and topless sisters.
+
+                Provide constructive criticism: When your friend does make a mistake, try to give him constructive feedback on what he could have done differently.
+                Don't focus on the negative or blame him for the loss, but rather offer suggestions for improvement.
+
+                Help him identify his strengths and weaknesses: Ask your friend what champions and playstyles he feels most comfortable with.
+                If he's struggling in top lane, it might be worth exploring other roles or champions that better suit his playstyle.
+                Additionally, help him identify areas where he can improve, such as last-hitting minions or better map awareness.
+
+                Remind him to stay positive: Negative attitudes can be contagious, and a defeatist mindset can quickly spread throughout the team.
+                Encourage your friend to stay positive and focus on what he can do to turn the game around, rather than dwelling on mistakes or losses.
+
+                Play together: Playing with friends can be a great way to alleviate stress and improve communication.
+                Queue up with your friend and offer him support during the game. This can also help him feel more comfortable in the top lane, knowing that he has a friend to rely on.
+
+                Remember, everyone has bad games, and it's important to support your friend rather than criticizing him. Offer constructive feedback and encouragement,
+                and remind him that the goal is to have fun and improve, not necessarily to win every game.</p>
         </>
     )
 }
